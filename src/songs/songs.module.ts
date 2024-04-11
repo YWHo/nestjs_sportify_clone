92826ca8344658a2ce59332f39042a68f625498c
Brian Ho <YWHo@users.noreply.github.com>
2024-04-11 23:29:21 +1200

    different ways to call provider (see commented code)

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,8 +2,31 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+// const mockSongsService = {
+//   findAll() {
+//     return [{ id: 1, title: 'Lasting lever' }];
+//   },
+// };
+
 @Module({
   controllers: [SongsController],
-  providers: [SongsService]
+  providers: [
+    SongsService, // "standard" provider syntax
+  ],
+  // providers: [
+  //   // "useClass" provider syntax
+  //   {
+  //     provide: SongsService,
+  //     useClass: SongsService,
+  //   },
+  // ],
+  // providers: [
+  //   // "useValue" provider syntax (allow mock data)
+  //   SongsService,
+  //   {
+  //     provide: SongsService,
+  //     useValue: mockSongsService,
+  //   },
+  // ],
 })
 export class SongsModule {}
